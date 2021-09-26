ebf9d1532dbb0af4eee29cfc06a23d6ae2e22298
Eva <shepherdlord1119@gmail.com>
2021-09-26 17:03:08 +0800

    Initial commit

---
+++ src/views/front/About.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container-fluid px-3 px-md-0 pb-5">
+    <header>
+      <h1 class="fs-4">關於NATURE</h1>
+       <div
+        class="container header-img fade-out"
+        style="
+          background-image: url('https://i.imgur.com/rfdVSxo.jpg');
+          background-repeat: no-repeat;
+          background-position: center;
+          background-size: cover;
+          height: 15vh;
+          min-height: 20rem;
+        "
+         ></div>
+  </header>
+   <div class="container-fluid px-3 px-md-0 pb-5 d-flex
+    justify-content-start flex-wrap" style="width:90%">
+     <div>
+    <h1 class="fs-4 mt-5">品牌理念</h1>
+    <p class="m-4 flex">自然X舒適的時裝搭配，充分享受自由的生活，創造屬於自己的穿搭樂趣</p>
+     </div>
+     <div>
+    <h1 class="fs-4 mt-5 mr-4">訂單注意事項</h1>
+    <ul class="aboutFlex m-4">
+      <li>付款方式：信用卡刷卡、取貨付款</li>
+      <li>運送方式：宅配、便利商店取貨(全家、7-11、萊爾富)</li>
+      <li>退貨：七日內可退貨，商品包裝需完整復原才可退回，若有破損、污漬等狀況無法退回</li>
+      <li>客服專線：(02)12345678</li>
+    </ul>
+     </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+    };
+  },
+};
+</script>
